2560c831ecdffdabaea84f99e7bfd744cbfaff6f
Valdo <gkorchun@gmail.com>
2023-02-14 12:01:54 +0200

    Set app statefull

--- src/components/container/container.js
+++ src/components/container/container.js
@@ -1,22 +1,38 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState } from 'react';
+
 import Header from '../header';
 import Section from '../section';
 import List from '../list';
 import Form from '../form';
+
 import './container.scss';
 
-const Container = () => (
-    <Fragment>
-        <Header />
-        <main>
-            <Section headingText='Add a new favourite'>
-                <Form />
-            </Section>
-            <Section headingText='Records'>
-                <List />
-            </Section>
-        </main>
-    </Fragment>
-);
+const Container = () => {
+    const [records, setRecords] = useState([]),
+
+    onSubmit = (entry) => {
+        setRecords(
+            [...records, entry].sort((a, b) => {
+                if (a.recordName > b.recordName) return 1;
+                if (a.recordName < b.recordName) return -1;
+                return 0;
+            }) // sorting array before setting it on state guaranty it's run ones when the state is calculated
+        )
+    };
+
+    return (
+        <Fragment>
+            <Header />
+            <main>
+                <Section headingText='Add a new favourite'>
+                    <Form onSubmit={ onSubmit } />
+                </Section>
+                <Section headingText='Records'>
+                    <List records={ records } />
+                </Section>
+            </main>
+        </Fragment>
+    )
+};
 
 export default Container;
