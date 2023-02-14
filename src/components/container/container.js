5f4c4a1d1d8a0e2243b2ba00fefd87b97fdefa18
Valdo <gkorchun@gmail.com>
2023-02-14 12:03:29 +0200

    Dabbling in accessibility

--- src/components/container/container.js
+++ src/components/container/container.js
@@ -9,28 +9,36 @@ import './container.scss';
 
 const Container = () => {
     const [records, setRecords] = useState([]),
+        [liveText, setLiveText] = useState(''),
 
-    onSubmit = (entry) => {
-        setRecords(
-            [...records, entry].sort((a, b) => {
-                if (a.recordName > b.recordName) return 1;
-                if (a.recordName < b.recordName) return -1;
-                return 0;
-            }) // sorting array before setting it on state guaranty it's run ones when the state is calculated
-        )
-    };
+        onSubmit = (entry) => {
+            setRecords(
+                [...records, entry].sort((a, b) => {
+                    if (a.recordName > b.recordName) return 1;
+                    if (a.recordName < b.recordName) return -1;
+                    return 0;
+                }) // sorting array before setting it on state guaranty it's run ones when the state is calculated
+            );
+            setLiveText(`${ entry.recordName } successfully added.`)
+        };
 
     return (
         <Fragment>
             <Header />
             <main>
-                <Section headingText='Add a new favourite'>
+                <Section headingText='Add a new favourite' >
                     <Form onSubmit={ onSubmit } />
                 </Section>
-                <Section headingText='Records'>
+                <Section headingText='Records' >
                     <List records={ records } />
                 </Section>
             </main>
+            <div
+                className='visually-hidden'
+                aria-live='polite'
+                aria-atomic='true' >
+                { liveText }
+            </div>
         </Fragment>
     )
 };
